1f56200b5c62a8cefe39e6a038c01e46bc0f697c
zdm7788 <54230699+zhudimeng@users.noreply.github.com>
2025-10-29 16:58:29 +0800

    Set default NAME to 'home' in index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiYzJkMjI5NmQ4YmIzODY1MGVjZTg5
 const ARGO_PORT = process.env.ARGO_PORT || 8001;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
 const CFIP = process.env.CFIP || 'cdns.doon.eu.org';        // 节点优选域名或优选ip  
 const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
-const NAME = process.env.NAME || '';                        // 节点名称
+const NAME = process.env.NAME || 'home';                        // 节点名称
 
 // 创建运行文件夹
 if (!fs.existsSync(FILE_PATH)) {
